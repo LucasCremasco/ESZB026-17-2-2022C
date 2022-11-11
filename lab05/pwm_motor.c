2ec5033a41bce26a0764b669bdf72ae3651c1c99
weslleylks <83431453+weslleylks@users.noreply.github.com>
2022-11-11 17:07:15 -0300

    V13
    Duty cycle = 2050

--- lab05/pwm_motor.c
+++ lab05/pwm_motor.c
@@ -19,13 +19,13 @@ int main() {                            // este programa deve ser rodado com 'su
    // frequencia PWM = 19,2 MHz / (divisor * range)
    // 261,6 = 19200000 / (divisor * 128) => divisor = 15
    pwmSetMode(PWM_MODE_MS);             // usando frequencia fixa
-   pwmSetRange(1);                    // passos do duty cycle (max=4096)
+   pwmSetRange(2050);                    // passos do duty cycle (max=4096)
    pwmSetClock(21);                     // fornece uma frequencia de 10kHz (max=4096)
    printf("Iniciando...\n");
    //for(ciclos = 0; ciclos < 2; ciclos++){  // variando o duty cycle
     //for(dc = 0; dc < 100; dc++){
-        pwmWrite(pino_PWM0, 0);
-	printf("dc: %d\n", 0);
+        pwmWrite(pino_PWM0, 2050);
+	printf("dc: %d\n", 2050);
         usleep(10000000);
 	      
 	//      usleep(100000);
